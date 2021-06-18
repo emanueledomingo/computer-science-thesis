756196c3553340c552e534131965c7ad33459576
Emanuele Domingo <emanu.domingo@gmail.com>
2021-06-18 13:35:02 +0200

    fixed bug on attribute type

--- scripts/load_dataset.sql
+++ scripts/load_dataset.sql
@@ -5,15 +5,8 @@ CREATE TABLE IF NOT EXISTS online_retail (
     Quantity     BIGINT, 
     InvoiceDate  TEXT, 
     Price        DOUBLE, 
-    CustomerID  DOUBLE, 
+    CustomerID  TEXT, 
     Country      TEXT 
 );
 
 COPY online_retail FROM '/data/raw/online_retail_II_x1.csv';
-
-CREATE TABLE IF NOT EXISTS stock_codes (  
-    StockCode    TEXT, 
-    Price        DOUBLE
-);
-
-COPY stock_codes FROM '/data/raw/online_retail_II_indexes.csv';
